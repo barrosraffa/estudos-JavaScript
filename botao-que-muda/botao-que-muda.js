e9cb24bdd15717c0234f3fb383f652e5a20ceade
barrosraffa <rafaellaponci@gmail.com>
2020-03-23 14:13:08 -0300

    botão troca img quando clica

--- botao-que-muda/botao-que-muda.js
+++ botao-que-muda/botao-que-muda.js
@@ -1,8 +1,21 @@
-let clickButton = document.querySelector("#clickButton");
- 
-    clickButton.addEventListener('click', trocaImagem, false);
+let elementoButton = document.querySelector("#clickButton");
+//estado inicial da imagem é false, sem cor
+let estadoImagem = false; 
+elementoButton.addEventListener('click', trocaImagem);
 
+// se não tiver clicado no botão não mude a img se clicar muda
     function trocaImagem(){
         console.log('help')
-        document.getElementById("trocar-img").src="ice-cream-click.png";
+        let elementoImagem = document.getElementById("trocar-img")
+        if(!estadoImagem){
+            elementoImagem.src="ice-cream-click.png"
+            estadoImagem = true;
+        }
+        else {
+            elementoImagem.src="./ice-cream.png"
+            estadoImagem = false;
+        }
     }
+
+// se tiver clicado bota essa img, se nao bota essa
+// if () src="./ice-cream.png"
