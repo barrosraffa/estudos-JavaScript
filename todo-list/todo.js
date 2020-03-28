11055179a40578cd1e99b0c0331fede56b0844fb
barrosraffa <rafaellaponci@gmail.com>
2020-03-28 01:56:15 -0300

    java script iniciado

---
+++ todo-list/todo.js
@@ -0,0 +1,12 @@
+let botaoAdd = document.getElementById('botaoAdd');
+let listaTarefas = document.getElementById('listaTarefas');
+let inputDigitarTarefa = document.getElementById('digitarTarefa')
+
+botaoAdd.addEventListener('click', botaoAdicionar)
+
+function botaoAdicionar() {
+    console.log("aaaaaaa")
+    let tarefa = document.createElement("li")
+    tarefa.className = "icones";
+}
+
