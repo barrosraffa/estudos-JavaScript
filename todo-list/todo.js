929ca966705aa1b858eb12e546bf598833986348
barrosraffa <rafaellaponci@gmail.com>
2020-03-28 21:27:17 -0300

    adicionando tarefas - ok

--- todo-list/todo.js
+++ todo-list/todo.js
@@ -1,12 +1,23 @@
 let botaoAdd = document.getElementById('botaoAdd');
 let listaTarefas = document.getElementById('listaTarefas');
-let inputDigitarTarefa = document.getElementById('digitarTarefa')
+let inputDigitarTarefa = document.getElementById('digitarTarefa');
 
 botaoAdd.addEventListener('click', botaoAdicionar)
 
+botaoAdd.addEventListener('click', function(){
+    console.log(inputDigitarTarefa.value)
+    if(inputDigitarTarefa.value.length == 0){
+        alert("Digite uma tarefa!")
+        return false;
+    }
+});
+
+//adicionando tarefas
 function botaoAdicionar() {
     console.log("aaaaaaa")
-    let tarefa = document.createElement("li")
-    tarefa.className = "icones";
+    let tarefaLi = document.createElement("li")
+    tarefaLi.innerHTML = inputDigitarTarefa.value //ve o valor do meu input e se tiver maior que 0 ele adiciona no html
+    listaTarefas.appendChild(tarefaLi)    
 }
+
 
