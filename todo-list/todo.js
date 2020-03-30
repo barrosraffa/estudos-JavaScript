604ead02b5dd8649c2eef11001af5a56a43b4d4a
barrosraffa <rafaellaponci@gmail.com>
2020-03-30 17:39:46 -0300

    atualização de hover css : ok

--- todo-list/todo.js
+++ todo-list/todo.js
@@ -1,6 +1,9 @@
 let botaoAdd = document.getElementById('botaoAdd');
 let listaTarefas = document.getElementById('listaTarefas');
 let inputDigitarTarefa = document.getElementById('digitarTarefa');
+let botaoTarefaConcluida = document.getElementById('botaoTarefaConcluida');
+let btnDesmarcarTodas = document.getElementById('btnDesmarcarTodas');
+let btnExcluirTodas = document.getElementById('btnExcluirTodas');
 
 botaoAdd.addEventListener('click', botaoAdicionar)
 
@@ -19,16 +22,14 @@ function botaoAdicionar() {
     //adicionando tarefas
     let tarefaLi = document.createElement("li")
     tarefaLi.innerHTML = inputDigitarTarefa.value //ve o valor do meu input e se tiver maior que 0 ele adiciona no html
-    listaTarefas.appendChild(tarefaLi)    
-
+    listaTarefas.appendChild(tarefaLi)
 
     //limpando o input de tarefas
     inputDigitarTarefa.value = ' '
 
-
-    //deletando tarefas
+    //removendo uma tarefa
     let deletarTarefa = document.createElement("span")
-    deletarTarefa.innerText = "delete"
+    deletarTarefa.innerText = "x"
     tarefaLi.appendChild(deletarTarefa)
 
     deletarTarefa.addEventListener("click" , function(){
@@ -36,14 +37,9 @@ function botaoAdicionar() {
     })
 
     // concluindo uma tarefa
-    // removendo uma tarefa
+}
+    // deletando tarefas 
     // concluindo todas as tarefas
     // desmarcando todas as tarefas
     // removendo todas as tarefas
     // Adicionar o atributo que permite a movimentação
-}
-
-
-
-
-
