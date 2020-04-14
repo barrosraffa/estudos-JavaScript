45d7bf0b586dd1826807260ccde30483faa11b3d
barrosraffa <rafaellaponci@gmail.com>
2020-04-14 16:01:38 -0300

    começando a lógica

--- estilos-de-selecao-btn/script.js
+++ estilos-de-selecao-btn/script.js
@@ -1,24 +1,61 @@
-let btnSelecionarR = document.getElementById('btnSelecionarR')
 let SelecionarTres = document.getElementById('SelecionarTres')
 let SelecionarSoUm = document.getElementById('SelecionarSoUm')
 let SelecionarLivre = document.getElementById('SelecionarLivre')
 
-btnSelecionarR.addEventListener('click' , selecionaBtn)
+let btnSelecionarRafa = document.getElementById('btnSelecionarRafa')
+let btnSelecionarDiego = document.getElementById('btnSelecionarDi')
+let btnSelecionarLara = document.getElementById('btnSelecionarLara')
+let btnSelecionarCarol = document.getElementById('btnSelecionarCarol')
 
-let estadoDoBtn = false; 
+let btnSelecionarUm = document.getElementById('btnSelecionarUm')
+let btnSelecionarDois = document.getElementById('btnSelecionarDois')
+let btnSelecionarTres = document.getElementById('btnSelecionarTres')
+let btnSelecionarQuatro = document.getElementById('btnSelecionarQuatro')
 
-function selecionaBtn (){
+let btnSelecionarA = document.getElementById('btnSelecionarA')
+let btnSelecionarB = document.getElementById('btnSelecionarB')
+let btnSelecionarC = document.getElementById('btnSelecionarC')
+let btnSelecionarD = document.getElementById('btnSelecionarD')
+
+btnSelecionarRafa.addEventListener('click', BotaoSelecionarLivre)
+btnSelecionarDiego.addEventListener('click', BotaoSelecionarLivre)
+btnSelecionarLara.addEventListener('click', BotaoSelecionarLivre)
+btnSelecionarCarol.addEventListener('click', BotaoSelecionarLivre)
+
+btnSelecionarUm.addEventListener('click', BotaoSelecionarSoUm)
+btnSelecionarDois.addEventListener('click', BotaoSelecionarSoUm)
+btnSelecionarTres.addEventListener('click', BotaoSelecionarSoUm)
+btnSelecionarQuatro.addEventListener('click', BotaoSelecionarSoUm)
+
+btnSelecionarA.addEventListener('click', BotaoSelecionarTres)
+// btnSelecionarB.addEventListener('click', BotaoSelecionarTres)
+// btnSelecionarC.addEventListener('click', BotaoSelecionarTres)
+// btnSelecionarD.addEventListener('click', BotaoSelecionarTres)
+
+let estadoDaDiv = false; 
+
+//SELECIONAR ATÉ 3
+function BotaoSelecionarTres(){
     console.log('ouviu')
-
-    if(!estadoDoBtn) {
-        btnSelecionarR.classList.add("btn-selecionado")
-        estadoDoBtn = true;
+    if(!estadoDaDiv) {
+        btnSelecionarA.classList.add('btn-selecionado')
+        estadoDaDiv = true
+    }
+    else {
+        btnSelecionarA.classList.remove('btn-selecionado')
+        estadoDaDiv = false
     }
 }
 
-//SELECIONAR ATÉ 3
-function SelecionarTres(){}
 //SELECIOAR APENAS 1
-function SelecionarSoUm(){}
+function BotaoSelecionarSoUm(){
+    console.log('deu certo')
+}
+
 //SELECIONAR QUANTOS QUISER
-function SelecionarLivre(){}
+function BotaoSelecionarLivre(){
+    console.log('isso ai')
+}
+
+//validar as sections, se não estiver selecionada não deixar ir pra próxima etapda
+// disabled = false;
